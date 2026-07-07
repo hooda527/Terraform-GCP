114b1f410bc69a5672fd0b0706b86cc7dc9e3e1f
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 14:01:16 +0530

    Add Google provider configuration to Terraform

--- provider.tf
+++ provider.tf
@@ -1,1 +1,15 @@
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
