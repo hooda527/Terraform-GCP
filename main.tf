2ec2072c2b13b8fd2abe5efb7291251a9ad12485
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 20:56:26 +0530

    Fix Terraform project structure

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+variable "instance_name" {
+  description = "Name of the VM instance"
+  type        = string
+  default     = "my-instance" # Provide a default or ensure it's passed via tfvars or environment variables
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM instance"
+  type        = string
+  default     = "e2-medium" # A common default machine type
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = var.machine_type
