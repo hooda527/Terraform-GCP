eecc8d035499b9477f790ea4122a10951009dc8b
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 21:00:34 +0530

    Remove duplicate variable declarations from provider.tf
    - Keep provider configuration only in provider.tf
    - Variable definitions are already in variables.tf
    - Fixes Terraform validation error about duplicate 'region' variable

--- provider.tf
+++ provider.tf
@@ -3,18 +3,3 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
-
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP region"
-  type        = string
-}
-
-variable "zone" {
-  description = "GCP zone"
-  type        = string
-}
