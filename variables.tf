c7856d94c9aa6feaf500b537dc572b1e4ab8f048
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 21:02:29 +0530

    Add missing variable declarations for zone, instance_name, and machine_type

--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "region" {
   type        = string
   default     = "asia-south1"
 }
+
+variable "zone" {
+  description = "Google Cloud Zone"
+  type        = string
+  default     = "asia-south1-a"
+}
+
+variable "instance_name" {
+  description = "Name of the VM instance"
+  type        = string
+  default     = "terraform-instance"
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM instance"
+  type        = string
+  default     = "e2-micro"
+}
