f61c4bbd753063d4fad8b89131befb174ad8dc55
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 20:13:17 +0530

    Add GitHub Actions and project structure

--- provider.tf
+++ provider.tf
@@ -1,30 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-}
-
-// Declare variables used by the provider
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP region"
-  type        = string
-}
-
-variable "zone" {
-  description = "GCP zone"
-  type        = string
 }
