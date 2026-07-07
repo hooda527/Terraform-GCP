babeea6eee131814a44ade476df15286217c7761
Ehshanul Hooda <hoodaehshanual@gmail.com>
2026-07-07 14:01:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
 
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  default = "asia-south1"
+}
+
+variable "zone" {
+  default = "asia-south1-a"
+}
+
+variable "instance_name" {
+  default = "terraform-vm"
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
